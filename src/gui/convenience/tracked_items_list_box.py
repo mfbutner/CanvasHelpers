82909c04309d55526c43b0f2784398ba2456f925
Janet Z <jqhzhang@ucdavis.edu>
2020-09-19 08:17:05 -0700

    Restored all the stuff I accidentally modified back :( Sorry!

--- src/gui/convenience/tracked_items_list_box.py
+++ src/gui/convenience/tracked_items_list_box.py
@@ -1,5 +1,5 @@
 import tkinter
-from typing import Iterable, Union, overload, Callable, TypeVar
+from typing import Iterable, Union, overload, Callable, TypeVar, Optional
 
 T = TypeVar('T')
 
@@ -86,7 +86,8 @@ class TkinterDisplayableList(list):
 
 
 class TrackedItemListBox(tkinter.Listbox):
-    def __init__(self, master=None, values=None, to_string: Callable[[T], str] = str, cnf={}, **kw):
+    def __init__(self, master=None, values: Optional[Iterable] = None, to_string: Callable[[T], str] = str, cnf={},
+                 **kw):
         super().__init__(master, cnf, **kw)
         self._values = TkinterDisplayableList(values, to_string) if values is not None \
             else TkinterDisplayableList(to_string=to_string)
