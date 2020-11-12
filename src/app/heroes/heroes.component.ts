fcdaa1f0a86e55bb13f7bdf0695226076c3fba7f
PlazmaZero <Howardw117@gmail.com>
2020-11-11 19:41:43 -0800

    Commented and added a onSelect element.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,26 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  /* set heroes list to elements from HEROES */
   heroes = HEROES;
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  /* selected hero */
+  selectedHero: Hero;
+
   constructor() { 
   }
 
   ngOnInit(): void {
   }
 
+  /* each hero element has an id and name */
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  /* on select assigns the clicked hero from the template to selectedHero */
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
