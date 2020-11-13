078f133daa93d9bbd6972fa408ef4d6731b83583
PlazmaZero <Howardw117@gmail.com>
2020-11-12 18:00:45 -0800

    Added imports and get function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
+  /* we declare that this service should be  created 
+  by the root application injector. */
   providedIn: 'root'
 })
 export class HeroService {
+  
+  constructor() {}
 
-  constructor() { }
+  getHeros(): Hero[] {
+    return HEROES;
+  }
+
 }
