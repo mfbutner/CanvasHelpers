7837329647fb41c479e95ff5a6e6f44c98d3d8ea
PlazmaZero <Howardw117@gmail.com>
2020-11-16 22:21:16 -0800

    Routed herodetail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 /* displays heroes component to /heroes URL */
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
-  {path: 'dashboard', component: DashboardComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
