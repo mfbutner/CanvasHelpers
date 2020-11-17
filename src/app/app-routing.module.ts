9c233393ce914519a53cd15a8cec3839694dd20f
PlazmaZero <Howardw117@gmail.com>
2020-11-16 22:11:12 -0800

    Added dashboard and default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
 
 /* displays heroes component to /heroes URL */
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
