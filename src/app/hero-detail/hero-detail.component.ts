b891a01fe054e884f0a721a15a62157d45341025
PlazmaZero <Howardw117@gmail.com>
2020-11-18 18:24:19 -0800

    Save values into private fields

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,7 +12,11 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
    
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
   }
