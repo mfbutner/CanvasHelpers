e1574efd4a940451a35139fa378d0001766652b2
PlazmaZero <Howardw117@gmail.com>
2020-11-18 18:39:21 -0800

    Add back button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,10 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
   /* defines an input property of type Hero */
   @Input() hero:Hero;
 
