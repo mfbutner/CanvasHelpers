2d434e47fdeb49b8ee53159bbb1f474afe7e1650
PlazmaZero <Howardw117@gmail.com>
2021-02-15 01:31:30 -0800

    Cleaned up studentClass

--- GroupingStudents1/studentClass.py
+++ GroupingStudents1/studentClass.py
@@ -1,69 +1,52 @@
 #!/usr/bin/env python3
 # this is a class containing student information
 class Student:
-    #Personal data
-    idNum = 0
-    name = "default_name"
-    firstName = "default_firstName"
-    lastName = "default_lastName" 
-    schoolEmail = "default@gmail.com"
-    
-    #Pronouns
-    pronouns = "default/defaults" #string representing the pronoun
-    preferSame = 1 #does the person prefer someone of their same gender.
-
-    #Meet the times (starting [sunday] [midnight])
-    meetingTimes = [[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False], [False, False, False, False, False, False]]
-    preferAsy = 0 # 3 values, 0 = sync, 1= no preference, 2=async
-
-    #Contact Infonrmation
-    contactPreference = [False, False, False, True]  #Discord, Phone Number, Email, Canvas Groups 0 = no, 1= no preference, 2= yes
-    contactInformation = ["defaultDiscordName#2424", "(000) 000-0000", "deafultEmail@gmail.com"]
-
-    #Preferred Leader or Follower
-    preferLeader = False
-
-    #Country of Origin if US then false?
-    countryOfOrigin = "United States of America"
-    preferCountry = False
-
-    # international student preference
-    international = 0  
-         
-    #Language Preference
-    language = "English"
-    
-    #Preffered Stuff to Do
-    option1 = "default"
-    option2 = "default"
-    freeResponse = "default"
-
-    #Priority
-    priorityList = ["default", "default", "default", "default", "default"]
-
-    #confidence
-    confidence = "default"
     
     def __init__(self, idNumInput = 0, nameInput = "default_name", emailInput = "default_email", firstNameInput = "default_firstName", lastNameInput = "default_lastName"):
+        # Student identification information
         self.name = nameInput
         self.idNum = idNumInput
         self.schoolEmail = emailInput
-        self.pronouns = "default/defaults"
-        self.preferSame = 1
-        self.meetingTimes = [[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False], [False, False, False, False, False, False]]
-        self.preferAsy = 0
-        self.contactPreference = [False, False, False, True]
-        self.contactInformation = ["defaultDiscordName#2424", "(000) 000-0000", "deafultEmail@gmail.com"]
-        self.preferLeader = False
-        self.international = 0
-        self.language = "English"
-        self.option1 = "default"
-        self.option2 = "default"
-        self.freeResponse = "default"
-        self.priorityList = ["default", "default", "default", "default", "default"]
-        self.confidence = "default"
         self.firstName = firstNameInput
         self.lastName = lastNameInput
+
+        # Student preferred pronouns
+        self.pronouns = "default/defaults"
+        # If student prefers the same pronouns
+        # 0 - Not same pronouns 1 - No preference 2 - Prefer the same pronouns
+        self.preferSame = 1
+
+        # Student available meeting times in PST
+        # [Day][time] in 4 hour in hr intervals from 12am to 12am starting Sunday
+        self.meetingTimes = [[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False],[False, False, False, False, False, False], [False, False, False, False, False, False]]
+
+        # Does the student prefer to meet asynchronously
+        # 0 - No preference 1 - Synchronous 2 - Asynchronous
+        self.preferAsy = 0
+
+        # contact preference and information
+        self.contactPreference = [False, False, False, True]
+        self.contactInformation = ["defaultDiscordName#2424", "(000) 000-0000", "deafultEmail@gmail.com"]
+
+        # If student prefers being the leader
+        self.preferLeader = False
+
+        # Is the student international and would they like to be with anothe international student
+        # 0 - not international 1 - no preference 2 - is international and would like to match
+        self.international = 0
+
+        # Language preference
+        self.language = "English"
+
+        # What the student prefers to do.
+        self.option1 = "default"
+        self.freeResponse = "default"
+
+        # The what the student prioritizes in a study group most to least.
+        self.priorityList = ["default", "default", "default", "default", "default"]
+
+        # How confident the student feels (0 - 2) default is 1
+        self.confidence = 1
     
     def __str__(self): 
         fullString = ""
@@ -81,7 +64,7 @@ class Student:
         fullString += "\nContactInformation:"
         for value in self.contactInformation:
             fullString += " " + value + ", "
-        fullString += "\npreferLeader: " + str(self.preferLeader) + " international: " + str(self.international) +" language: " + self.language + " option1: " + self.option1 + " option2: " + self.option2 + " freeresponse: " + self.freeResponse
+        fullString += "\npreferLeader: " + str(self.preferLeader) + " international: " + str(self.international) +" language: " + self.language + " option1: " + self.option1 + " freeresponse: " + self.freeResponse
         fullString += "\npriorityList: "
         for value in self.priorityList:
             fullString += " " + value + ", "
