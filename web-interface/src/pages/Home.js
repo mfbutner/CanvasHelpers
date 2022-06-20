c6ee903733f22b822f7ef05df120b7d665372d04
Marvin Lee <marvinlee.github@gmail.com>
2022-06-20 15:56:18 -0700

    Created React App and customized some folders and text in it. Removed unnecessary files

---
+++ web-interface/src/pages/Home.js
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default Home;
