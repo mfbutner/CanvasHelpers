e077d48d5b48cea8413ea23b16f8de0d49a67be2
Marvin Lee <marvinlee.github@gmail.com>
2022-06-24 03:36:09 -0700

    WIP for mapping questions back together

--- StableGrouping/main.py
+++ StableGrouping/main.py
@@ -4,38 +4,23 @@ from makeGroups import makeGroups
 from checkValidGroup import invalidGroupDict
 from analyzeCode import gradeGroups
 from sendCanvasConvo import sendConvo
-from canvasapi import Canvas
 from canvasapi.canvas_object import CanvasObject
 from studentClass import Student
-from parseStudent import parse, parseEmails, parsePartnerQuiz
+from parseStudent import parse, parseEmails, parsePartnerQuiz, parse_submissions
+import json
 from dotenv import dotenv_values
 
+# Load .env file with environment variables from project root
 env = dotenv_values("../.env")
-
-def retrieveCSVfromCanvas(quiz:CanvasObject) :
-       studentReport = quiz.create_report("student_analysis")
-       reportProgress = None
-       # URL of canvas progress object from studentReport
-       reportProgressURL = studentReport.progress_url
-       # parse so only the process id remains
-       reportProgressID = reportProgressURL.removeprefix('https://canvas.ucdavis.edu/api/v1/progress/')
-
-       # wait for student report to finish generating while the process has not completed or failed
-       while reportProgress != 'completed' and reportProgress != 'failed':
-              reportProgressObj = canvas.get_progress(reportProgressID)
-              reportProgress = reportProgressObj.workflow_state
-              print(reportProgress)
-       studentReportN = quiz.create_report("student_analysis")
-       url = studentReportN.file["url"]
-       studentData = pd.read_csv(url)
-
-       return studentData
-
-#Get the right class
 API_URL = env["API_URL"]
 API_KEY = env["API_KEY"]
 
-#Macros that will have to change to the appropriate class and survey number
+# Load config file for this session
+config_file = open("../config.json")
+config = json.load(config_file)
+config_file.close()
+
+# Macros that will have to change to the appropriate class and survey number
 
 # ECS 36A
 # CLASS_ID = 574775 # ECS 36A
@@ -43,43 +28,32 @@ API_KEY = env["API_KEY"]
 # className = "ECS 36A"
 
 # ECS 36B
-CLASS_ID = 569280 #36B
-QUIZ_ID = 123095 # ECS 36B main quiz
-className = "ECS 36B"
+# CLASS_ID = 569280 #36B
+# QUIZ_ID = 123095 # ECS 36B main quiz
+# className = "ECS 36B"
 # QUIZ_ID2 = 1 # who do you want to be with quiz
 
-studyGroupNumber = "Fifth Study Group"
+# studyGroupNumber = "Fifth Study Group"
 
-
-#Get the class data from canvas
+# Use API Key and URL to authenticate upcoming Canvas API Calls
 canvas = Canvas(API_URL, API_KEY)
-canvasClass = canvas.get_course(CLASS_ID)
 
-# Get the right quiz
-quiz = canvasClass.get_quiz(QUIZ_ID)
-studentReport = quiz.create_report("student_analysis")
-reportProgress = None
+# Obtain selected course from the Canvas API
+course = canvas.get_course(config.course.id)
 
-# URL of canvas progress object from studentReport
-reportProgressURL = studentReport.progress_url
+# Get a list of all students enrolled in the course
+students = course.get_users()
 
-# parse so only the process id remains
-reportProgressID = reportProgressURL.removeprefix('https://canvas.ucdavis.edu/api/v1/progress/')
-
-# wait for student report to finish generating while the process has not completed or failed
-while reportProgress != 'completed' and reportProgress != 'failed':
-       reportProgressObj = canvas.get_progress(reportProgressID)
-       reportProgress = reportProgressObj.workflow_state
-
-studentReportN = quiz.create_report("student_analysis")
-url = studentReportN.file["url"]
-studentData = pd.read_csv(url)
+# Get selected Canvas quiz to match partners with
+quiz = course.get_quiz(config.quiz_id)
 
 #Parse the student data of those that took the survey
-dictStudentTakeSurvey = parse(studentData, CLASS_ID, canvasClass)
+students_submitted = parse_submissions(students, quiz, config)
+
+dictStudentTakeSurvey = parse(studentData, config.course.id, course)
 
 #Finds out who did not take survey (also updates the entire class with their school emails)
-dictStudentDidNotTakeSurvey = parseEmails(dictStudentTakeSurvey, canvasClass)
+dictStudentDidNotTakeSurvey = parseEmails(dictStudentTakeSurvey, course)
 
 #Find the appropriate quiz
 #quiz2 = canvasClass.get_quiz(QUIZ_ID2)
@@ -87,14 +61,14 @@ dictStudentDidNotTakeSurvey = parseEmails(dictStudentTakeSurvey, canvasClass)
 #parsePartnerQuiz(partnerQuizData ,canvasClass, dictStudentTakeSurvey, dictStudentDidNotTakeSurvey)
 
 #Find the people who were matchedBefore, place it into a dict
-matchedBefore = invalidGroupDict(canvas, CLASS_ID)
+matchedBefore = invalidGroupDict(canvas, config.course.id)
 
 #Create the groups:
 groups = makeGroups(dictStudentTakeSurvey, dictStudentDidNotTakeSurvey, matchedBefore)
 
 
 #Now that groups are matched, send emails and form groups
-sendConvo(canvas, CLASS_ID, groups, studyGroupNumber)
+sendConvo(canvas, config.course.id, groups, config.group_number)
 
 #Anaylze the groups: how many students with a preference got it?
 gradeGroups(groups, matchedBefore)
